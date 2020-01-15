b75cba0cefa9b163e2fd88f76c5b8c0185938003
Eli Zevin <dizzyjaguar@gmail.com>
2020-01-14 16:53:09 -0800

    add random integer function and console log to quick test it

--- app.js
+++ app.js
@@ -4,7 +4,16 @@ const guessAnswer = document.getElementById('guess-answer');
 const triesSpan = document.getElementById('tries-span');
 const winLoseSpan = document.getElementById('win-lose');
 
+// function for getting a random integer up to a maximum number
+function randomInt(maximum) {
+    return Math.floor(Math.random() * Math.floor(maximum));
+}
+// the number to guess will be generated randomly up to the number 33
+const numberToGuess = randomInt(33);
+//checking that this works, I should write a test though
+console.log (numberToGuess);
 
 
+//this is where my button event listener will be once i write the remaining functions
 
-guessButton.addEventListener('click' )
+//guessButton.addEventListener('click' )
