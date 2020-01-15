aee9f4a86032711efaff6b06480def6aae2667df
Eli Zevin <dizzyjaguar@gmail.com>
2020-01-14 16:40:54 -0800

    get most of the Elements in my app.js file

---
+++ app.js
@@ -0,0 +1,10 @@
+const numberInput = document.getElementById('number-input');
+const guessButton = document.getElementById('guess-button');
+const guessAnswer = document.getElementById('guess-answer');
+const triesSpan = document.getElementById('tries-span');
+const winLoseSpan = document.getElementById('win-lose');
+
+
+
+
+guessButton.addEventListener('click' )
