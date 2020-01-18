3350114b3e22677e1ad863c1baf3ec355b941fe7
Eli Zevin <dizzyjaguar@gmail.com>
2020-01-18 12:23:41 -0800

    all functionality working, just needs css now

--- app.js
+++ app.js
@@ -4,34 +4,35 @@ const numberInput = document.getElementById('number-input');
 const guessButton = document.getElementById('guess-button');
 const guessAnswer = document.getElementById('guess-answer');
 const triesSpan = document.getElementById('tries-span');
-const winLoseSpan = document.getElementById('win-lose');
+
 
 // function for getting a random integer up to a maximum number
 function randomInt(maximum) {
     return Math.floor(Math.random() * Math.floor(maximum));
 }
-// the number to guess will be generated randomly up to the number 33
+
 let numberToGuess = randomInt(33);
-//checking that this works, I should write a test though
-
-
-
-//this is where my button event listener will be once i write the remaining functions
+let tries = 9;
 
 guessButton.addEventListener('click', () => {
     let humanInput = Number(numberInput.value);
-
     let allGuesses = compareNumbers(humanInput, numberToGuess);
-    
-    
+    const loseMsg = ('Your all out of tries, YOU LOSE');
+
+    tries--;
+    triesSpan.textContent = tries;
 
     if (allGuesses === 0) {
         guessAnswer.textContent = ('You are a genius!');
+        guessButton.disabled = true;
     } else if (allGuesses === 1) {
         guessAnswer.textContent = ('A little too high buddy...');
     } else if (allGuesses === -1) {
         guessAnswer.textContent = ('Too low...');
     }
 
-    console.log(compareNumbers(humanInput, numberToGuess));
+    if (tries === 0) {
+        alert (loseMsg);
+        guessButton.disabled = true;
+    }
 });
